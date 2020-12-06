bbe9086146040e3ff29ad596643b873304405122
pradeep.pp <pradeep.pp@gmail.com>
2020-12-06 11:35:11 -0500

    delete product test completed

--- src/test/java/com/qaguru/batch19shop/services/ProductService.java
+++ src/test/java/com/qaguru/batch19shop/services/ProductService.java
@@ -2,8 +2,12 @@ package com.qaguru.batch19shop.services;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.qaguru.batch19shop.models.Product;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.filter.log.LogDetail;
 import io.restassured.http.ContentType;
+import io.restassured.internal.RequestSpecificationImpl;
 import io.restassured.response.ValidatableResponse;
+import io.restassured.specification.RequestSpecification;
 import org.apache.http.HttpStatus;
 import org.testng.Assert;
 
@@ -17,6 +21,16 @@ public class ProductService {
     private ObjectMapper objectMapper = new ObjectMapper();
     private String baseUri = "http://localhost:8090";
     private String basePath = "/api/v1/products";
+    private RequestSpecification specification;
+
+    public ProductService() {
+        specification = new RequestSpecBuilder()
+                .setBaseUri(baseUri)
+                .setBasePath(basePath)
+                .setContentType(ContentType.JSON)
+                .log(LogDetail.ALL)
+                .build();
+    }
 
     public Product readProductDetails(String file) {
         URL url = getClass().getClassLoader().getResource(file);
@@ -78,5 +92,16 @@ public class ProductService {
         Product resProduct = getResponse.extract().body().as(Product.class);
         product.setId(resProduct.getId());
         Assert.assertEquals(resProduct,product,"Incorrect product details");
+    }
+
+    public void deleteService(String productId) {
+        given()
+                .spec(specification)
+                .when()
+                .delete("/"+productId)
+                .then()
+                .log().all()
+                .assertThat().statusCode(HttpStatus.SC_NO_CONTENT);
+
     }
 }
